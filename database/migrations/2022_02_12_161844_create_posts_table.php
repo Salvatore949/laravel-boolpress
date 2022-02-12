ab3d9ff066b2f27ea88362c3de0d82d9a3371056
Salvatore949 <salvo.mess49@gmail.com>
2022-02-12 19:46:15 +0100

    migration,foreign key insert

---
+++ database/migrations/2022_02_12_161844_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('titolo',200);
+            $table->string('sottotitolo',350);
+            $table->text('testo');
+            $table->string('autore',60);
+            $table->date('data');
+            $table -> bigInteger('category_id') ->unsigned();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
