3e5afe83e31c055724822493006a5a9a725ab01d
Salvatore949 <salvo.mess49@gmail.com>
2022-02-14 00:03:40 +0100

    data printed in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,25 +3,34 @@
 @section('main')
 
 <body>
-   <h1>Posts</h1> 
+   <h1>Posts</h1>
+   <table border="2">
+     <tr>
+        <th>titolo</th>
+        <th>sottotitolo</th>
+        <th>testo</th>
+        <th>autore</th>
+        <th>data</th>
+        <th>category</th>
+        <th>tags</th>
+        <th>created_at</th>
+     </tr>
    @foreach($posts as $post)
-   <table>
-
       <tr>
-
-   <td>{{$post -> titolo}} </td> 
-   <td>{{$post -> sottotitolo}}</td> 
-   <td>{{$post -> testo}}</td> 
-   <td>{{$post -> autore}}</td> 
-   <td>{{$post -> data}}</td> 
-   <td>{{$post -> category -> name}}</td>
-   <td></td>
-   <td>{{$post -> created_at -> format('d/m/Y - H:m')}}</td>  <br>
-    
+         <td>{{$post -> titolo}} </td> 
+         <td>{{$post -> sottotitolo}}</td> 
+         <td>{{$post -> testo}}</td> 
+         <td>{{$post -> autore}}</td> 
+         <td>{{$post -> data}}</td> 
+         <td>{{$post -> category -> nome}}</td>
+         <td>
+            @foreach($post -> tags as $tag)
+            {{$tag -> nome}}
+            @endforeach
+         </td>
+         <td>{{$post -> created_at -> format('d/m/Y - H:m')}}</td>  <br>
     @endforeach
-
       </tr>
-
     </table>
 </body>
 
