28b8953ae24afb6eeba04c6fe9366dd94103229e
Salvatore949 <salvo.mess49@gmail.com>
2022-02-13 12:50:12 +0100

    routes created

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             PostSeeder::class,
-            // TagSeeder::class,
+            TagSeeder::class,
         ]);
     }
 }
